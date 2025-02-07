fda1efe1cff7d0d0ab726b7caf9cff45e71c78b4
PHILIP OBIORAH <philip.obiorah@outlook.com>
2025-02-07 09:07:57 +0000

    should we play

--- adventure.py
+++ adventure.py
@@ -2,4 +2,13 @@ name = input("What is your name? ")
 
 print("Hello, ", name, "Welcome to the adventure game")
 
-should_we_play = input("Do you want to play the game? (yes/no) ")
+should_we_play = input("Do you want to play the game? (yes/no) ").lower()
+
+if should_we_play == "yes" or should_we_play == "y":
+    print("We gonna play")
+
+elif should_we_play == "YESS":
+    print("WE ARE GONNA PLAY")
+
+else:
+    print("We are  NOT playing....")
